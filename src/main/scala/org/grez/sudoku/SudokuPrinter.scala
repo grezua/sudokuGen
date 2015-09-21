cb91f0ce6312eda9e35e9651da185e6dfa930ef0
Andrii Zhdanov <andrii.zhdanov@globallogic.com>
2015-09-21 21:43:20 +0300

    Hide numbers

--- src/main/scala/org/grez/sudoku/SudokuPrinter.scala
+++ src/main/scala/org/grez/sudoku/SudokuPrinter.scala
@@ -1,5 +1,7 @@
 package org.grez.sudoku
 
+import scala.util.Random
+
 /**
  * Created by grez on 9/18/15.
  */
@@ -10,17 +12,31 @@ object SudokuPrinter {
 
 
   def main(args: Array[String]) {
-    printField(testField)
+    val printedList = hideCells(testField, 100)
+
+    printField(printedList)
   }
 
-  def printField(field: List[List[Int]]): Unit = {
+  def hideCells(field: List[List[Int]], hiddenCount: Int): List[List[Option[Int]]] = {
+    val numbers = (for (i <- 1 to hiddenCount) yield Random.nextInt(81)).toList.distinct
+
+    val result = for {i <- field.indices }
+      yield {
+        for {j <- field(i).indices} yield if (numbers.contains(i+(j*9))) None else Some(field(i)(j))
+      }.toList
+
+    result.toList
+
+  }
+
+  def printField(field: List[List[Option[Int]]]): Unit = {
 
     for (i <- field.indices) {
       if(i % 3 == 0) println(spacer) else println(hr)
       for (j <- field(i).indices){
         if(j % 3 == 0) print("|")
         print("|")
-        print(s" ${field(i)(j)} ")
+        print(s" ${field(i)(j).getOrElse(" ")} ")
       }
       print("||")
       println()
